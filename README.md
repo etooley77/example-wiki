15ebbd193a48b7a122611a2bf1934af784e2cbf6
etooley77 <tooleye77@gmail.com>
2024-03-09 13:34:05 -0500

    basic necessities

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## DjangoBase
 A starter project including login/registration for users and a mod system once you add a 'Moderators' group to the admin page.
+
+# NOTE : Before you run the server, create a superuser, then log in to the admin page. Create a group called 'Moderators' to make sure you do not get any errors in your localhost.
--- site_main/settings.py
+++ site_main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,11 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'C:/Users/toole/OneDrive/Desktop/VS CODE (1)/django_apps/wiki/static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/fonts'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = '/media/'
